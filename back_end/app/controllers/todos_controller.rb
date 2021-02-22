660e3873ff44bae6bde5b967f9347781a059768b
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-02-22 23:44:22 +0900

    feat: 目覚まし音声にするために，Todoリストを作成

--- back_end/app/controllers/todos_controller.rb
+++ back_end/app/controllers/todos_controller.rb
@@ -1,6 +1,34 @@
 class TodosController < ApplicationController
+  def index
+    todos = Todo.all.map { |todo| {id: todo.id, content: todo.content} }
+    render json: todos
+  end
+
   def create
     todo = Todo.new(todo_params)
-    render json: { data: todo }
+    if todo.save
+      render json: todo
+    end
+  end
+
+  def show
+    todo = Todo.find(params[:id])
+    render json: todo
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(content: params[:content])
+  end
+
+  def destroy
+    id = params[:id]
+    Todo.find(id).destroy
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:content)
   end
 end
--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,24 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { TodoList } from './Todolist'
+import ReactModal from 'react-modal';
 
 function App() {
+  const [todoList, setTodoList] = useState([] as TodoList[]);
+  const [value, setValue] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [editId, setEditId] = useState(-1);
+
+  function getTodos() {
+    axios.get(`http://localhost:3001/todos`)
+      .then(res => {
+        setTodoList(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getTodos();
+  },[setTodoList]);
+
+  function handleCreate() {
+    const todo = {
+      content: value
+    }
+
+    axios.post('http://localhost:3001/todos', { todo })
+      .then(() => {
+        getTodos();
+        setValue('');
+      })
+  };
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+
+  function handleDelete(id: number): void {
+    axios.delete('http://localhost:3001/todos/' + id)
+      .then(() => {
+        getTodos();
+      });
+  };
+
+  function handleEdit(id: number):void {
+    axios.get('http://localhost:3001/todos/' + id)
+      .then(res => {
+        setValue(res.data['content']);
+      });
+    setIsOpen(true);
+    setEditId(id);
+  }
+
+  function handleUpdate() {
+    axios.patch('http://localhost:3001/todos/' + editId, {'content': value})
+      .then(() => {
+        getTodos();
+      });
+    setValue('');
+    setEditId(-1);
+    setIsOpen(false);
+  }
+
+  function handleCancel() {
+    setValue('');
+    setIsOpen(false);
+  }
+
+  const todoListNode = todoList.map((element: TodoList) => {
+    return (
+      <li key={element.id}>
+        {element.content}
+        <button onClick={() => handleEdit(element.id)}>編集</button>
+        <button onClick={() => handleDelete(element.id)}>削除</button>
+      </li>
+    )
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <ReactModal
+          isOpen={isOpen}
+          ariaHideApp={false}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type='text'
+            value={value}
+            onChange={handleChange}
+          />
+          <button onClick={() => handleUpdate()}>編集</button>
+          <button onClick={() => handleCancel()}>キャンセル</button>
+        </ReactModal>
+        <h1>Todoリスト一覧</h1>
+        <ul>
+          {todoListNode}
+        </ul>
+      </div>
+      <div>
+        <h1>新規作成</h1>
+        <input 
+          type="text"
+          value={value}
+          onChange={(e) => handleChange(e)}
+        />
+        <button onClick={() => handleCreate()}>作成</button>
+      </div>
+      
     </div>
   );
 }
