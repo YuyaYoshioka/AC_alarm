c62ec36f8f7a42fcde6cfa71081c2b07974967af
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-02-25 00:18:47 +0900

    feat: アラーム機能作成

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   const [value, setValue] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [editId, setEditId] = useState(-1);
+  const [alarmTime, setAlarmTime] = useState('09:00');
+  const [isAlarmOn, setIsAlarmOn] = useState('false');
+  const [userID, setUserID] = useState('');
 
   function getTodos() {
     axios.get(`http://localhost:3001/todos`)
@@ -19,7 +22,18 @@ function App() {
 
   useEffect(() => {
     getTodos();
-  },[setTodoList]);
+    const cookies = document.cookie; 
+    const cookiesArray = cookies.split(';'); 
+    for(const for_cookie of cookiesArray) { 
+        const split_cookie = for_cookie.split('=');
+        if (split_cookie[0].trim() === 'isAlarmOn') {
+          setIsAlarmOn(split_cookie[1]);
+        }
+        if (split_cookie[0].trim() === 'userID') {
+          setUserID(split_cookie[1]);
+        }
+    }
+  },[]);
 
   function handleCreate() {
     const todo = {
@@ -32,10 +46,6 @@ function App() {
         setValue('');
       })
   };
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setValue(e.target.value);
-  }
 
   function handleDelete(id: number): void {
     axios.delete('http://localhost:3001/todos/' + id)
@@ -68,6 +78,84 @@ function App() {
     setIsOpen(false);
   }
 
+  function handleSetAlarm() {
+    var contents: string = '';
+    axios.get(`http://localhost:3001/todos`)
+      .then(res => {
+        for(const toDo of res.data) {
+          contents += toDo['content'] + 'を頑張りましょう。';
+        }
+        contents += '起きてください。'
+        const repeatContents: string = contents.repeat(1000);
+        const speakContent = new SpeechSynthesisUtterance(repeatContents);
+        const alarmTimeNumber:number = Number(alarmTime.slice(0, 2)) * 3600 + Number(alarmTime.slice(-2)) * 60;
+        const currentDatetime = new Date();
+        const currentTime:number = currentDatetime.getHours() * 3600 + currentDatetime.getMinutes()* 60 + currentDatetime.getSeconds();
+        var restTime:number = 0;
+        if (alarmTimeNumber > currentTime) {
+          restTime = (alarmTimeNumber - currentTime);
+        } else {
+          restTime = (24*3600 + alarmTimeNumber -currentTime)
+        }
+        const alarmGetTime = currentDatetime.getTime() + restTime*1000;
+        const speak = function(){
+          speechSynthesis.speak(speakContent);
+        };
+        var timeoutID;
+        timeoutID = setTimeout(speak, restTime*1000);
+        document.cookie = 'timeoutID=' + timeoutID;
+        document.cookie = 'alarmGetTime=' + alarmGetTime;
+        document.cookie = 'isAlarmOn=true';
+      });
+  };
+
+  function handleCancelAlarm() {
+    const cookies = document.cookie; 
+    const cookiesArray = cookies.split(';'); 
+    var timeoutID;
+    for(const for_cookie of cookiesArray) { 
+        const split_cookie = for_cookie.split('=');
+        if (split_cookie[0].trim() === 'timeoutID') {
+          timeoutID = split_cookie[1];
+        }
+    }
+    clearTimeout(Number(timeoutID));
+    document.cookie = 'isAlarmOn=false';
+  };
+
+  function handleStopAlarm() {
+    const url = "https://kenkoooo.com/atcoder/atcoder-api/results?user=" + userID;
+    axios.get(url)
+      .then(res => {
+        var acTime = res.data[0].epoch_second
+        for (const problem of res.data) {
+          if (problem.result !== 'AC') {
+            continue;
+          }
+          if (problem.epoch_second > acTime) {
+            acTime = problem.epoch_second;
+          }
+        }
+        var cookies = document.cookie; 
+        var cookiesArray = cookies.split(';'); 
+        var alarmGetTime: string = '';
+        for(var for_cookie of cookiesArray){ 
+            var split_cookie = for_cookie.split('=');
+            if (split_cookie[0].trim() === 'alarmGetTime') {
+              alarmGetTime = split_cookie[1];
+            }
+        }
+        if (Number(alarmGetTime) < Number(acTime)*1000) {
+          speechSynthesis.cancel();
+        }
+      }
+    )
+  }
+  
+  function handleUserID() {
+    document.cookie = 'userID=' + userID;
+  }
+
   const todoListNode = todoList.map((element: TodoList) => {
     return (
       <li key={element.id}>
@@ -80,6 +168,20 @@ function App() {
 
   return (
     <div>
+      <header>
+        <h1>AtCoder Alarm</h1>
+        <input
+          type='text'
+          onKeyPress={(e): void => {
+            if (e.key === 'Enter') {
+              handleUserID();
+            }
+          }}
+          value={userID}
+          onChange={(e): void => setUserID(e.target.value)}
+        />
+      </header>
+
       <div>
         <ReactModal
           isOpen={isOpen}
@@ -88,26 +190,39 @@ function App() {
           <input
             type='text'
             value={value}
-            onChange={handleChange}
+            onChange={(e): void => setValue(e.target.value)}
           />
           <button onClick={() => handleUpdate()}>編集</button>
           <button onClick={() => handleCancel()}>キャンセル</button>
         </ReactModal>
+
         <h1>Todoリスト一覧</h1>
         <ul>
           {todoListNode}
         </ul>
       </div>
+
       <div>
         <h1>新規作成</h1>
         <input 
-          type="text"
+          type='text'
           value={value}
-          onChange={(e) => handleChange(e)}
+          onChange={(e): void => setValue(e.target.value)}
         />
         <button onClick={() => handleCreate()}>作成</button>
       </div>
-      
+
+      <div>
+        <h1>起床時刻</h1>
+        <input
+          type='time'
+          value={alarmTime}
+          onChange={(e): void => setAlarmTime(e.target.value)}
+        />
+        <button onClick={() => handleSetAlarm()}>アラームをセットする</button>
+        <button onClick={() => handleCancelAlarm()}>アラームを解除する</button>
+        <button onClick={() => handleStopAlarm()}>アラームを止める</button>
+      </div>
     </div>
   );
 }
